fe693cfcfbfa1f8d04a69187f4115a68022ad783
RosieOh <20172207@gm.hannam.ac.kr>
2024-12-20 18:03:37 +0900

    FEAT : 터미널, 관리자, 메인페이지 URL 패턴 등록

--- serverDash/urls.py
+++ serverDash/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from terminal import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('terminal/', include('terminal.urls')),
+    path('', views.main_page, name='main_page'), 
 ]
