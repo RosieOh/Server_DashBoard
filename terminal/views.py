8b852b1b6027a99a12d3975e1cd01601c00b7e62
RosieOh <20172207@gm.hannam.ac.kr>
2024-12-20 18:05:38 +0900

    FEAT : AWS EC2 인스턴스 시작 및 종료 View 패턴 추가

--- terminal/views.py
+++ terminal/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .utils import describe_instances, start_instance, stop_instance
 
-# Create your views here.
+# EC2 인스턴스 대시보드
+def ec2_dashboard(request):
+    instances = describe_instances()  # EC2 인스턴스 상태 조회
+    print(instances)  # 추가: 템플릿으로 넘겨지는 instances 값 확인
+    return render(request, 'terminal/ec2_dashboard.html', {'instances': instances})
+
+# EC2 인스턴스 시작
+def start_instance_view(request, instance_id):
+    start_instance(instance_id)  # EC2 인스턴스 시작
+    return HttpResponse(f"Instance {instance_id} started.")
+
+# EC2 인스턴스 종료
+def stop_instance_view(request, instance_id):
+    stop_instance(instance_id)  # EC2 인스턴스 종료
+    return HttpResponse(f"Instance {instance_id} stopped.")
+
+def main_page(request):
+    return render(request, 'main.html')
